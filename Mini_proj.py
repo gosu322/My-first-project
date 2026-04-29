e3d235456ff7a752fb28da956199af4a4f64b3e4
gosu322 <rusel2502999@gmail.com>
2026-04-29 17:29:21 +0300

    corrected variant(final)

--- Mini_proj.py
+++ Mini_proj.py
@@ -1,7 +1,7 @@
 import random
 
-counter = 0
-number = random.randint(1, 100)
+
+
 print("Добро пожаловать в числовую угадайку")
 
 
@@ -10,24 +10,35 @@ def is_valid(value):
         return 1 <= int(value) <= 100
     return False
 
-game = input("Хотите сыграть?(да/нет)\n")
-while game == "да":
-    user_input = input("Введите целое число от 1 до 100\n")
-    if not is_valid(user_input):
-        print("А может быть все-таки введем целое число от 1 до 100?")
-        continue
 
-    user_num = int(user_input)
-    if user_num < number:
-        print("Ваше число меньше загаданного, попробуйте ещё разок")
+game = input("Хотите сыграть?(да/нет)\n")
+
+while game == "да":
+    number = random.randint(1, 100)
+    counter = 0
+    print("Я загадал целое число от 1 до 100")
+    while True:
+        user_input = input("Введите целое число от 1 до 100\n")
+
+        if not is_valid(user_input):
+            print("А может быть все-таки введем целое число от 1 до 100?")
+            continue
+
+        user_num = int(user_input)
         counter += 1
-    elif user_num > number:
-        print("Ваше число больше загаданного, попробуйте ещё разок")
-        counter += 1
-    else:
-        print("Вы угадали, поздравляем!")
-        print(f'Попыток сделано: {counter}')
-        game = input("Спасибо, что сыграли в числовую угадайку! Хотите сыграть ещё?(да/нет)\n")
-        if game == "нет":
-            print("Очень жаль. Ждём вас снова!")
+        if user_num < number:
+            print("Ваше число меньше загаданного, попробуйте ещё разок")
+        elif user_num > number:
+            print("Ваше число больше загаданного, попробуйте ещё разок")
+        else:
+            print("Вы угадали, поздравляем!")
+            print(f"Попыток сделано: {counter}")
             break
+
+    game = input(
+        "Спасибо, что сыграли в числовую угадайку! Хотите сыграть ещё?(да/нет)\n"
+    )
+    if game == "нет":
+        print("Очень жаль. Ждём вас снова!")
+            
+        
